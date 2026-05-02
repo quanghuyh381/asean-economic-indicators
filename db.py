62fbc008a6da6c70eaeedf158438800bf0d11ea3
quanghuyh381 <quanghuyh381@gmail.com>
2026-05-02 23:58:44 +0700

    fix db connection for railway

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
 import psycopg2
+import os
 
 def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+    if database_url:
+        return psycopg2.connect(database_url)
     return psycopg2.connect(
-        host="localhost",
-        dbname="ASEAN_Macroeconomics_Indicators",
-        user="postgres",
-        password="huy2712",
-        port=2712
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
     )
